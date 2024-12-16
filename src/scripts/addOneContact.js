8cde0344d599a699ce2fae6e79570f912bbec636
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2024-12-16 13:55:16 +0100

    homework -1 comple.

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,19 @@
-export const addOneContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+
+export const addOneContact = async () => {
+    try {
+        const contacts = await readContacts();
+
+        const newContacts = Array.from({ length: 1 }, createFakeContact);
+
+        const updatedContacts = [...contacts, ...newContacts];
+
+        await writeContacts(updatedContacts);
+    } catch (err) {
+        console.error('Помилка генерації контактів:', err);
+    }
+};
 
 addOneContact();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,17 @@
-export const removeLastContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+    try {
+        const contacts = await readContacts();
+
+        contacts.pop();
+
+        await writeContacts(contacts);
+    } catch (err) {
+        console.log('Помилка видалення останнього контакта:', err);
+
+    }
+};
 
 removeLastContact();
